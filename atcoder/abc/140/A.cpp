fc007779b3aefd9bbd0950d62e7be9f326abcae1
rikuo haga <rikuohg@gmail.com>
2020-07-24 12:46:39 +0900

    EASY

---
+++ atcoder/abc/140/A.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//for loop関係
+#define rep(i,a,n) for(int i=(a);i<(n);++i)
+#define repe(i,a,n) for(int i=(a);i<=(n);++i)
+#define rrep(i,n,a) for(int i=(n);n>(a);--i)
+#define rrepe(i,n,a) for(int i=(n);n>=(a);--i)
+
+//略記
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+#define ENDL << "\n"
+
+using ll = long long;
+using P = pair<int,int>;
+using itn = int;
+
+//定数
+const int INF = 1<<30;
+const ll MOD = 1000000007;
+const long double PI =(acos(-1));
+
+/***スニペット***
+ifelse, isprime, torad, todeg, lcm, bfs
+****************/
+
+//隣接リスト
+vector<int> to[100005];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+
+    cout << n*n*n ENDL;
+    
+    return 0;
+}
