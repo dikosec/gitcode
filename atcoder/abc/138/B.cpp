afa2fa32ad805479d9ee0e72fe9fa9d5e5b6fdf9
rikuo haga <rikuohg@gmail.com>
2020-07-25 01:13:31 +0900

    editAC floating point number

---
+++ atcoder/abc/138/B.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//for loop関係
+#define rep(i,a,n) for(int i=(a);i<(n);++i)
+#define repe(i,a,n) for(int i=(a);i<=(n);++i)
+#define rrep(i,n,a) for(int i=(n);n>(a);--i)
+#define rrepe(i,n,a) for(int i=(n);n>=(a);--i)
+
+//略記
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+#define ENDL << "\n"
+
+using ll = long long;
+using P = pair<int,int>;
+using itn = int;
+
+//定数
+const int INF = 1<<30;
+const ll MOD = 1000000007;
+const long double PI =(acos(-1));
+
+/***スニペット***
+ifelse, isprime, torad, todeg, lcm, bfs
+****************/
+
+//隣接リスト
+vector<int> to[100005];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    double ans=0.0,A;
+    rep(i,0,n){
+        cin >> A;
+        ans+=1.0/A;
+    }
+    cout << fixed << setprecision(20);
+    cout << (long double)1.0/ans ENDL;
+    
+    return 0;
+}
