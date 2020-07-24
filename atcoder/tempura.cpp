4501cee41148e33439cb4341d00f1adf61d07c28
rikuo haga <rikuohg@gmail.com>
2020-07-25 01:25:49 +0900

    to english

--- atcoder/tempura.cpp
+++ atcoder/tempura.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//for loop関係
+//for loop
 #define rep(i,a,n) for(int i=(a);i<(n);++i)
 #define repe(i,a,n) for(int i=(a);i<=(n);++i)
 #define rrep(i,n,a) for(int i=(n);n>(a);--i)
 #define rrepe(i,n,a) for(int i=(n);n>=(a);--i)
 
-//略記
+//abbreviati
 #define PB push_back
 #define MP make_pair
 #define F first
@@ -19,16 +19,16 @@ using ll = long long;
 using P = pair<int,int>;
 using itn = int;
 
-//定数
+//constant
 const int INF = 1<<30;
 const ll MOD = 1000000007;
 const long double PI =(acos(-1));
 
-/***スニペット***
+/***Snippet**************************************
 ifelse, isprime, torad, todeg, lcm, bfs
-****************/
+************************************************/
 
-//隣接リスト
+//adjacency list
 vector<int> to[100005];
 
 int main(){
