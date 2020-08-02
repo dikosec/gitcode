20233ab398508dc6d0af8dd1e8982a62184377eb
rikuo haga <rikuohg@gmail.com>
2020-08-03 07:27:46 +0900

    debug

---
+++ atcoder/abc/174/B_kaisetsu.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//for loop
+#define rep(i,a,n) for(int i=(a);i<(n);++i)
+#define repe(i,a,n) for(int i=(a);i<=(n);++i)
+#define rrep(i,n,a) for(int i=(n);n>(a);--i)
+#define rrepe(i,n,a) for(int i=(n);n>=(a);--i)
+
+//abbreviati
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+#define ENDL << "\n"
+
+using ll = long long;
+using P = pair<int,int>;
+using itn = int;
+
+//constant
+const int INF = 1<<30;
+const ll INFLL = 1LL << 60;
+const ll MOD = 1000000007;
+const long double PI =(acos(-1));
+
+/***Snippet***************************************************
+ifelse, isprime, torad, todeg, lcm, bfs,chmin,chmax
+**************************************************************/
+
+//adjacency list
+vector<int> to[100005];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    ll d;
+    cin >> n >> d;
+    int cnt=0;
+    rep(i,0,n){
+        ll x,y;
+        cin >> x >> y;
+        //sqrtは遅いのでdの方を二乗
+        if(x*x+y*y <= d*d) cnt++;
+    }
+    cout << cnt ENDL;
+    
+    return 0;
+}
