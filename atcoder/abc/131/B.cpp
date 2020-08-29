44996417bb95f156b6228a93e8df2284bde72f73
rikuo haga <rikuohg@gmail.com>
2020-08-30 03:28:32 +0900

    EASY

---
+++ atcoder/abc/131/B.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// for loop
+#define rep(i,a,n) for(int i=(a);i<(n);++i)
+#define repe(i,a,n) for(int i=(a);i<=(n);++i)
+#define rrep(i,n,a) for(int i=(n);n>(a);--i)
+#define rrepe(i,n,a) for(int i=(n);n>=(a);--i)
+
+// abbreviati
+#define PB(v) push_back(v)
+#define MP make_pair
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+#define ENDL << "\n"
+
+using ll = long long;
+using P = pair<int,int>;
+using itn = int;
+
+// constant
+const int INF = 1<<30;
+const ll INFLL = 1LL << 60;
+const ll MOD = 1000000007;
+const long double PI =(acos(-1));
+
+/***Snippet***************************************************
+ifelse, isprime, torad, todeg, lcm, bfs,chmin,chmax
+**************************************************************/
+
+// adjacency list
+vector<int> to[100005];
+
+// Graph
+using Graph = vector<vector<int>>;
+
+// Grid
+int H,W;
+vector<vector<int>> field;
+
+// 4方向への移動ベクトル
+const int dx[4]={1,0,-1,0};
+const int dy[4]={0,1,0,-1};
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n,l; cin >> n >> l;
+
+    int taste=0;
+    int ans=INF;
+    int sum=0;
+    int j=0;
+    repe(i,1,n){
+        taste=l+i-1;
+        if(ans!=min(ans,abs(taste))){
+            ans=abs(taste);
+            j=i;
+        }
+        sum+=taste;
+    }
+    sum-=l+j-1;
+    cout << sum ENDL;
+
+    return 0;
+}
