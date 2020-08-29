9a251d5bc6c2346796eebb6d999e6143ca3bcf3c
rikuo haga <rikuohg@gmail.com>
2020-08-30 05:19:51 +0900

    pair

---
+++ atcoder/abc/128/B.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// for loop
+#define rep(i,a,n) for(int i=(a);i<(n);++i)
+#define repe(i,a,n) for(int i=(a);i<=(n);++i)
+#define rrep(i,n,a) for(int i=(n);n>(a);--i)
+#define rrepe(i,n,a) for(int i=(n);n>=(a);--i)
+
+// abbreviati
+#define PB(v) push_back(v)
+#define MP(A,B) make_pair(A,B)
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+#define ENDL << "\n"
+
+using ll = long long;
+using P = pair<int,int>;
+using itn = int;
+
+// constant
+const int INF = 1<<30;
+const ll INFLL = 1LL << 60;
+const ll MOD = 1000000007;
+const long double PI =(acos(-1));
+
+/***Snippet***************************************************
+ifelse, isprime, torad, todeg, lcm, bfs,chmin,chmax
+**************************************************************/
+
+// adjacency list
+vector<int> to[100005];
+
+// Graph
+using Graph = vector<vector<int>>;
+
+// Grid
+int H,W;
+vector<vector<int>> field;
+
+// 4方向への移動ベクトル
+const int dx[4]={1,0,-1,0};
+const int dy[4]={0,1,0,-1};
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n; cin >> n;
+    vector<pair<string,pair<int,int>>> vp(n);
+    rep(i,0,n){
+        string s;
+        int p;
+        cin >> s >> p;
+        vp[i].first=s;
+        vp[i].second.first=-p;
+        vp[i].second.second=i+1;
+    }
+    sort(vp.begin(),vp.end());
+    rep(i,0,n){
+        cout << vp[i].second.second ENDL;
+    }
+    
+    return 0;
+}
