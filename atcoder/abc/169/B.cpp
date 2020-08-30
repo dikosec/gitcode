da220c6682d425c1646eedff5b85199598d0b30c
rikuo haga <rikuohg@gmail.com>
2020-08-30 17:30:13 +0900

    とてもムズイ　乗算でオーバーフローするかどうかは除算で確かめられる（移項する）

--- atcoder/abc/169/B.cpp
+++ atcoder/abc/169/B.cpp
@@ -1,25 +1,69 @@
-/*B*/
 #include<bits/stdc++.h>
 using namespace std;
+
+// for loop
+#define rep(i,a,n) for(int i=(a);i<(n);++i)
+#define repe(i,a,n) for(int i=(a);i<=(n);++i)
+#define rrep(i,n,a) for(int i=(n);n>(a);--i)
+#define rrepe(i,n,a) for(int i=(n);n>=(a);--i)
+
+// abbreviati
+#define PB(v) push_back(v)
+#define MP(A,B) make_pair(A,B)
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+#define ENDL << "\n"
+
+using ll = long long;
+using P = pair<int,int>;
+using itn = int;
+
+// constant
+const int INF = 1<<30;
+const ll INFLL = 1LL << 60;
+const ll MOD = 1000000007;
+const long double PI =(acos(-1));
+
+/***Snippet***************************************************
+ifelse, isprime, torad, todeg, lcm, bfs,chmin,chmax
+**************************************************************/
+
+// adjacency list
+vector<int> to[100005];
+
+// Graph
+using Graph = vector<vector<int>>;
+
+// Grid
+int H,W;
+vector<vector<int>> field;
+
+// 4方向への移動ベクトル
+const int dx[4]={1,0,-1,0};
+const int dy[4]={0,1,0,-1};
+
+const ll MX=1e18;
+
 int main(){
-    int N;
-    uint64_t A;
-    uint64_t ans=1;
-    uint64_t MAX=1000000000000000000;
-    cin >> N;
-    //int flag=0;
-    for(int i=0;i<N;i++){
-        cin >> A;
-        ans=(uint64_t)((double)ans*A);
-       
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n; cin >> n;
+    ll ans=1;
+    bool ng=0;
+    rep(i,0,n){
+        ll tmp;
+        cin >> tmp;
+        if(tmp==0){
+            cout << 0 ENDL;
+            return 0;
+        }
+        if(ng || (MX+1)/ans < tmp || ans*tmp > MX){
+            ng=1;
+        }
+        ans*=tmp;
     }
-
-    if(ans>MAX){
-        cout << -1 << endl; 
-    }
-    else{
-        cout << fixed << setprecision(0);
-        cout << ans << endl;
-    }
-
+    if(ng) cout << -1 ENDL;
+    else cout << ans ENDL;
+    return 0;
 }
