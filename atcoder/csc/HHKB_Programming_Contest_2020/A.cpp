4329fe31835987eb7be3d03422e7ac6c3daf85cb
rikuo haga <rikuohg@gmail.com>
2020-10-10 23:32:32 +0900

    ABC3kan

---
+++ atcoder/csc/HHKB_Programming_Contest_2020/A.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// for loop
+#define rep(i,a,n) for(int i=(a);i<(n);++i)
+#define repe(i,a,n) for(int i=(a);i<=(n);++i)
+#define rrep(i,n,a) for(int i=(n);n>(a);--i)
+#define rrepe(i,n,a) for(int i=(n);n>=(a);--i)
+
+// abbreviati
+#define PB(v) push_back(v)
+#define MP(A,B) make_pair(A,B)
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+#define ENDL << "\n"
+
+using ll = long long;
+using P = pair<int,int>;
+using itn = int;
+
+// constant
+const int INF = 1<<30;
+const ll INFLL = 1LL << 60;
+const ll MOD = 1000000007;
+const long double PI =(acos(-1));
+
+/***Snippet***************************************************
+ifelse, isprime, torad, todeg, lcm, bfs,chmin,chmax
+**************************************************************/
+
+// adjacency list
+vector<int> to[100005];
+
+// Graph
+using Graph = vector<vector<int>>;
+
+// Grid
+int H,W;
+vector<vector<int>> field;
+
+// 4方向への移動ベクトル
+const int dx[4]={1,0,-1,0};
+const int dy[4]={0,1,0,-1};
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+     char s,t;
+    cin >> s >> t;
+    if(s=='Y' && t=='a'){
+        cout << 'A' ENDL;
+    }
+    else if(s=='Y' && t=='b'){
+        cout << 'B' ENDL;
+    }
+    else if(s=='Y' && t=='c'){
+        cout << 'C' ENDL;
+    }
+    else cout << t ENDL;
+    
+    
+    
+    return 0;
+}
