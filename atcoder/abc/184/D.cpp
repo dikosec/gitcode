4d8b5dadb327e244e56bf0034222cb91c0f7e0ee
rikuo haga <rikuohg@gmail.com>
2020-11-22 23:41:22 +0900

    2kan

---
+++ atcoder/abc/184/D.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// for loop
+#define rep(i,a,n) for(int i=(a);i<(n);++i)
+#define repe(i,a,n) for(int i=(a);i<=(n);++i)
+#define rrep(i,n,a) for(int i=(n);n>(a);--i)
+#define rrepe(i,n,a) for(int i=(n);n>=(a);--i)
+
+// abbreviati
+#define PB(v) push_back(v)
+#define MP(A,B) make_pair(A,B)
+#define F first
+#define S second
+#define all(v) v.begin(),v.end()
+#define ENDL << "\n"
+
+using ll = long long;
+using P = pair<int,int>;
+using itn = int;
+
+// constant
+const int INF = 1<<30;
+const ll INFLL = 1LL << 60;
+const ll MOD = 1000000007;
+const long double PI =(acos(-1));
+
+/***Snippet***************************************************
+ifelse, isprime, torad, todeg, lcm, bfs,chmin,chmax
+**************************************************************/
+
+// adjacency list
+vector<int> to[100005];
+
+// Graph
+using Graph = vector<vector<int>>;
+
+// Grid
+int H,W;
+vector<vector<int>> field;
+
+// 4方向への移動ベクトル
+const int dx[4]={1,0,-1,0};
+const int dy[4]={0,1,0,-1};
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    double a,b,c;
+    cin >> a >> b >> c;
+    double sum=a+b+c;
+    double difa=100-a;
+    double difb=100-b;
+    double difc=100-c;
+    
+    double pa=a/sum;
+    double pb=b/sum;
+    double pc=c/sum;
+
+
+    double ans=((difa)*pa)+((difb)*pb)+((difc)*pc);
+
+    //cout << fixed << setprecision(20) << (difa*(a/sum)+difb*(b/sum)+difc*(c/sum)) ENDL;
+    
+    cout << fixed << setprecision(20) << ans ENDL;
+
+    return 0;
+}
---
+++ atcoder/abc/184/D.py
@@ -0,0 +1,7 @@
+a,b,c=[int(x) for x in input().split()]
+
+sum=a+b+c;
+
+ans=(69*(a)/sum)+(59*(b)/sum)+(41*(c)/sum);
+
+print(ans)
